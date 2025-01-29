2507932723ed44ef2db376856c9f5020840d55f4
Meadow <meadowingc@gmail.com>
2025-01-29 15:56:17 -0600

    add dev db seeds

--- db/seed.ts
+++ db/seed.ts
@@ -1,6 +1,21 @@
-import { db } from "astro:db";
+import { db, Site, User } from "astro:db";
 
-// https://astro.build/db/seed
-export default async function seed() {
-  // TODO
+export default async function () {
+  await db.insert(User).values([
+    {
+      id: 1,
+      name: "Meadow",
+      email: "meadowingc@proton.me",
+      createdAt: new Date(),
+    },
+  ]);
+
+  await db.insert(Site).values([
+    {
+      id: 1,
+      userId: 1,
+      url: "https://meadow.cafe",
+      createdAt: new Date(),
+    },
+  ]);
 }
