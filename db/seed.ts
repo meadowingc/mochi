b10903bd3d4eddd3740f814f047c093e84976c56
Meadow <meadowingc@gmail.com>
2025-01-30 16:20:53 -0600

    add chart!

--- db/seed.ts
+++ db/seed.ts
@@ -1,4 +1,4 @@
-import { db, Site, User } from "astro:db";
+import { db, Hit, Site, User } from "astro:db";
 
 export default async function () {
   await db.insert(User).values([
@@ -18,4 +18,23 @@ export default async function () {
       createdAt: new Date(),
     },
   ]);
+
+  const hits: {
+    siteId: number;
+    path: string;
+    date: Date;
+  }[] = [];
+  const paths = ["/", "/about", "/contact", "/products", "/blog"];
+  const now = Date.now();
+
+  for (let i = 0; i < 1000; i++) {
+    // Generate 1000 random visits
+    hits.push({
+      siteId: 1,
+      path: paths[Math.floor(Math.random() * paths.length)],
+      date: new Date(now - Math.floor(Math.random() * 7 * 24 * 60 * 60 * 1000)), // Random date within the last 7 days
+    });
+  }
+
+  await db.insert(Hit).values(hits);
 }
