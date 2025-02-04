71d10c034fce3d5e92e81301f10f585ade9b010a
Meadow <meadowingc@gmail.com>
2025-02-04 09:30:46 -0600

    in prod, minimize css

--- run_forever.sh
+++ run_forever.sh
@@ -3,7 +3,7 @@
 # run forever, even if we fail
 while true; do
     git pull
-    npx @tailwindcss/cli -i ./main.css -o ./assets/css/bundled_styles.css
+    npx @tailwindcss/cli -i ./main.css -o ./assets/css/bundled_styles.css --minify
     go build -tags release -o mochi
     ./mochi
     sleep 1
