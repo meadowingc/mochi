8038d416911aa03625107b1bdeec54bbaa9029ea
Meadow <meadowingc@gmail.com>
2025-02-11 11:27:36 -0600

    wal-checkpoint databases on close

--- database/db_wrapper.go
+++ database/db_wrapper.go
@@ -12,12 +12,11 @@ import (
 )
 
 const (
-	databaseFolder              = ".user_databases"
-	databaseNameFormat          = "mochi_%s.db"
-	databaseFilePathFormat      = databaseFolder + "/" + databaseNameFormat
-	databaseConnectionStrFormat = "file:" + databaseFilePathFormat + "?cache=shared&mode=rwc&_journal_mode=WAL"
-	cleanupInterval             = 10 * time.Minute    // Interval to run the cleanup process
-	cacheDuration               = cleanupInterval * 2 // Maximum duration to keep a database connection in cache
+	databaseFolder         = ".user_databases"
+	databaseNameFormat     = "mochi_%s.db"
+	databaseFilePathFormat = databaseFolder + "/" + databaseNameFormat
+	cleanupInterval        = 10 * time.Minute    // Interval to run the cleanup process
+	cacheDuration          = cleanupInterval * 2 // Maximum duration to keep a database connection in cache
 )
 
 type UserDb struct {
@@ -52,6 +51,11 @@ func (u *UserDb) close() {
 	if err != nil {
 		log.Printf("Error on closing database connection: %v", err)
 	} else {
+		// Perform a checkpoint to consolidate the WAL file into the main database file
+		if _, err := sqlDB.Exec("PRAGMA wal_checkpoint(FULL)"); err != nil {
+			log.Printf("Error on checkpointing database: %v", err)
+		}
+
 		if err := sqlDB.Close(); err != nil {
 			log.Printf("Error on closing database connection: %v", err)
 		}
@@ -127,7 +131,7 @@ func getCachedOrCreateDB(username string) *UserDb {
 
 	var err error
 	db, err := gorm.Open(sqlite.Open(
-		fmt.Sprintf(databaseConnectionStrFormat, username),
+		fmt.Sprintf("file:"+databaseFilePathFormat+"?cache=shared&mode=rwc&_journal_mode=WAL", username),
 	), &gorm.Config{})
 	if err != nil {
 		log.Fatalf("failed to connect database: %v", err)
