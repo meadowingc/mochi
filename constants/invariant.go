40cf1ca219aed8f07624f76009d3ca7cc26ac208
Meadow <meadowingc@gmail.com>
2025-04-07 12:53:06 -0600

    decrease maximum initial users

--- constants/invariant.go
+++ constants/invariant.go
@@ -3,5 +3,5 @@ package constants
 const (
 	APP_NAME       = "mochi"
 	LOCAL_PORT_NUM = "4738"
-	MAX_USERS      = 50 // Maximum number of users allowed to register. Set to 0 to disable
+	MAX_USERS      = 20 // Maximum number of users allowed to register. Set to 0 to disable
 )
